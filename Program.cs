f764c6a211ab4f74f46992de769a765bb69b54e4
m.r.tajvidi@gmail.com <$Sg*%x9wOhIoopJ@>
2024-10-20 20:56:31 -0700

    MySqrt is added

--- Program.cs
+++ Program.cs
@@ -5,6 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Square root of 26 is: {MySqrt(26)}");
+        }
+
+        public static int MySqrt(int x)
+        {
+            if (x < 2) return x;
+
+            var start = 0;
+            var end = x;
+            var middle = 0;
+            middle = start + (end - start) / 2;
+
+            while (start <= end)
+            {
+                middle = start + (end - start) / 2;
+
+                var multiplication = (long)middle * middle;
+                if (multiplication > x)
+                {
+                    end = middle - 1;
+                }
+                else if (multiplication < x)
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+            return end;
         }
     }
 }
